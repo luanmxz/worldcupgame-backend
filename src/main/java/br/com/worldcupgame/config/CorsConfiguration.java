64a2d887d5f11cf2e2bc114b17186c8da06425dd
luanmxz <luanmarcenemxz22@gmail.com>
2022-11-21 10:31:48 -0300

    change: cors configuration

--- src/main/java/br/com/worldcupgame/config/CorsConfiguration.java
+++ src/main/java/br/com/worldcupgame/config/CorsConfiguration.java
@@ -19,6 +19,11 @@ public class CorsConfiguration implements WebMvcConfigurer {
         	.allowedHeaders("*")
         	.allowedHeaders("Access-Control-Allow-Headers: Origin, Content-Type, Authorization")
             .allowedHeaders("Access-Control-Allow-Origin: *");
+        registry.addMapping("https://app-wcg22.herokuapp.com/oauth/token")
+	        .allowedMethods("*")
+	    	.allowedHeaders("*")
+	    	.allowedHeaders("Access-Control-Allow-Headers: Origin, Content-Type, Authorization")
+	        .allowedHeaders("Access-Control-Allow-Origin: *");
         registry.addMapping("https://wcg22.netlify.app/#/login")
 	        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
 	        .allowedHeaders("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS")
